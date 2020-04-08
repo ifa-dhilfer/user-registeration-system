ab52ffc9ac4707e1bea4eed05294876f63c8bbff
ifa-dhilfer <62695347+ifa-dhilfer@users.noreply.github.com>
2020-04-08 18:54:54 +0530

    Add files via upload

---
+++ save.php
@@ -0,0 +1,26 @@
+<?php
+$con=new mysqli("localhost","root","","ajax");
+$usercheck=$_POST["usercheck"];
+$passcheck=$_POST["passcheck"];
+$emailcheck=$_POST["emailcheck"];
+$birthdaydaycheck=$_POST["birthdaydaycheck"];
+$birthdaymonthcheck=$_POST["birthdaymonthcheck"];
+$birthdayyearcheck=$_POST["birthdayyearcheck"];
+$educationcheck=$_POST["educationcheck"];
+$nameoftheinstitutioncheck=$_POST["nameoftheinstitutioncheck"];
+$resumecheck=$_POST["resumecheck"];
+$gendercheck=$_POST["gendercheck"];
+
+$sql="insert into users (usercheck,passcheck,emailcheck,birthdaydaycheck,birthdaymonthcheck,birthdayyearcheck,educationcheck,nameoftheinstitutioncheck,resumecheck,gendercheck) values ('{$usercheck}','{$passcheck}','{$emailcheck}','{$birthdaydaycheck}','{$birthdaymonthcheck}','{$birthdayyearcheck}','{$educationcheck}','{$nameoftheinstitutioncheck}','{$resumecheck}','{$gendercheck}')";
+if($con->query($sql))
+{
+	echo "data saved...!";
+}
+else
+{
+	echo "error in saving data...!";
+}
+
+
+
+?>
